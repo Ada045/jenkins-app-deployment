5995a57e9648381b3153b811025866cc0e2d450b
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-21 21:43:31 +0100

    Enhance deploy function with Docker and SSH
    Updated deploy function to include Docker command and SSH execution.

--- script.groovy
+++ script.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
 
 def deploy() {
-    echo 'deploying application...'
+    def dockerCmd = 'docker run -p 8081:8081 -d ada045/java-app:1.1.2'
+    sshagent(['ec2-server-key']) {
+        sh "ssh -o StricitHostKeyChecking=no ec2-user@@3.124.6.251 ${dockerCmd}"
+    }
 }
 return this
