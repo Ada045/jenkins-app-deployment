7d2c7ea063743a9f984c91c54ce9203e6efb7ad3
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-22 14:49:22 +0100

    Refactor deploy function to use imageName variable
    Updated the deploy function to use a variable for the Docker image name.

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def deploy() {
-    def dockerCmd = 'docker run -p 8081:8081 -d ada045/java-app:1.1.2'
+    def dockerCmd = "docker run -p 8081:8081 -d ${imageName}"
     sshagent(['ec2-server-key']) {
         sh "ssh -o StricitHostKeyChecking=no ec2-user@63.177.99.170 ${dockerCmd}"
     }
