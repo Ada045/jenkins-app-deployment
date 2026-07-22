f52242fda9e6ae43a67038185d6368e54e5002d1
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-22 14:18:29 +0100

    Update SSH host in deploy function

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@
 def deploy() {
     def dockerCmd = 'docker run -p 8081:8081 -d ada045/java-app:1.1.2'
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StricitHostKeyChecking=no ec2-user@@3.124.6.251 ${dockerCmd}"
+        sh "ssh -o StricitHostKeyChecking=no ec2-user@63.177.99.170 ${dockerCmd}"
     }
 }
 return this
