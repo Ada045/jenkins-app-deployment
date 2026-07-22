1fb65ed2aed3f3940bccb92d234425c7b43027ac
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-22 15:34:25 +0100

    Refactor deploy function for Docker container management
    Updated the deploy function to pull the Docker image, stop and remove the existing container, and run a new container with the specified name.

--- script.groovy
+++ script.groovy
@@ -1,9 +1,19 @@
 #!/usr/bin/env groovy
 
 def deploy(String imageName) {
-    def dockerCmd = "docker run -p 8081:8081 -d ${imageName}"
+
+    def dockerCmd = """
+        docker pull ${imageName}
+        docker stop java-app || true
+        docker rm java-app || true
+        docker run -d --name java-app -p 8081:8081 ${imageName}
+    """
+
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@63.177.99.170 ${dockerCmd}"
+        sh """
+            ssh -o StrictHostKeyChecking=no ec2-user@63.177.99.170 '${dockerCmd}'
+        """
     }
 }
+
 return this
