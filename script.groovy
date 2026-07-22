7e1110bd0db725bbae3ff0f5ef8a2df3f476a434
Adaeze Igbokwe <igbokweadaeze04@gmail.com>
2026-07-22 15:29:15 +0100

    Fix typo in StrictHostKeyChecking option

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@
 def deploy(String imageName) {
     def dockerCmd = "docker run -p 8081:8081 -d ${imageName}"
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StricttHostKeyChecking=no ec2-user@63.177.99.170 ${dockerCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@63.177.99.170 ${dockerCmd}"
     }
 }
 return this
